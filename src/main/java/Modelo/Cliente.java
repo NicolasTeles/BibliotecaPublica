36df11f3c5785f66e0114d3084ab03498b7fb3f3
girafinos <felipeprinci@gmail.com>
2024-11-08 17:35:15 -0300

    atributos, setters e getters da classe "Cliente"

--- src/main/java/Modelo/Cliente.java
+++ src/main/java/Modelo/Cliente.java
@@ -1,4 +1,55 @@
 package Modelo;
 
 public class Cliente {
+    private String nome;
+    private String cpf;
+    private String email;
+    private String senha;
+
+    public Cliente(String nome, String cpf, String email, String senha) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public Cliente(){
+        this.nome = "";
+        this.cpf = "";
+        this.email = "";
+        this.senha = "";
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
 }
