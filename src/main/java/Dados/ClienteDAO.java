c36c1ef647d9092a5b648adb7581528087493e34
NicolasTeles <nicolasrmendoncat@gmail.com>
2024-11-10 17:44:31 -0300

    adicao da atualizacao de conta do cliente

--- src/main/java/Dados/ClienteDAO.java
+++ src/main/java/Dados/ClienteDAO.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 import Conexao.Conexao;
 
 public class ClienteDAO {
-    public boolean criarContaCliente(Cliente cliente){
+    public boolean criarConta(Cliente cliente){
         int retorno = 0;
         try(Connection conexao = Conexao.getConexao();){
             String SQL = "INSERT INTO bibliotecapublica.cliente(nome, email, senha, cpf) VALUES (?, ?, ?, ?)";
@@ -37,6 +37,24 @@ public class ClienteDAO {
 
         } catch (SQLException e){
             System.out.println("Erro ao deletar conta");
+            e.printStackTrace();
+        }
+        return (retorno > 0);
+    }
+
+    public boolean atualizaConta(Cliente cliente){
+        int retorno = 0;
+        try(Connection conexao = Conexao.getConexao();){
+            String SQL = "UPDATE bibliotecapublica.cliente SET nome=?, email=?, senha=? WHERE cpf=?";
+            PreparedStatement comando = conexao.prepareStatement(SQL);
+            comando.setString(1, cliente.getNome());
+            comando.setString(2, cliente.getEmail());
+            comando.setString(3, cliente.getSenha());
+            comando.setString(4, cliente.getCpf());
+            retorno = comando.executeUpdate();
+
+        } catch (SQLException e){
+            System.out.println("Erro ao atualizar conta");
             e.printStackTrace();
         }
         return (retorno > 0);
