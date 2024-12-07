0b0dd16aa6bedd36dfd1b109617acaa974113949
Gabriel Vaz Bernardini <gabrielbr1809@gmail.com>
2024-12-07 14:50:01 -0300

    Classe funcionario

---
+++ src/main/java/Modelo/Funcionario.java
@@ -0,0 +1,47 @@
+package Modelo;
+
+public class Funcionario {
+    private String nome;
+    private String cpf;
+    private String login;
+    private String senha;
+    private boolean Eadm;
+
+    public Funcionario(){
+        this.nome = "";
+        this.cpf = "";
+        this.login = "";
+        this.senha = "";
+        this.Eadm = false;
+    }
+
+    public Funcionario(String nome, String cpf, String login, String senha, boolean Eadm) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.login = login;
+        this.senha = senha;
+        this.Eadm = Eadm;
+    }
+
+    public String getNome() { return nome; }
+
+    public String getCpf() { return cpf; }
+
+    public String getLogin() { return login; }
+
+    public String getSenha() { return senha; }
+
+    public boolean getEadm() { return Eadm; }
+
+
+    public void setNome(String nome) { this.nome = nome; }
+
+    public void setCpf(String cpf) { this.cpf = cpf; }
+
+    public void setLogin(String login) { this.login = login; }
+
+    public void setSenha(String senha) { this.senha = senha; }
+
+    public void setEadm(boolean Eadm) { this.Eadm = Eadm; }
+
+}
