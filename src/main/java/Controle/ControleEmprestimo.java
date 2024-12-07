aeaa0f2a91623b77d1fd988ffba65b317bcd89ea
NicolasTeles <nicolasrmendoncat@gmail.com>
2024-12-07 15:22:08 -0300

    controle emprestimo

--- src/main/java/Controle/ControleEmprestimo.java
+++ src/main/java/Controle/ControleEmprestimo.java
@@ -13,13 +13,33 @@ public class ControleEmprestimo {
     public boolean criaEmprestimo(Emprestimo emprestimo){
         int retorno = 0;
         try(Connection conexao = Conexao.getConexao()){
-            String SQL = "INSERT INTO bibliotecapublica.emprestimo(data_inicial, vencimento, devolvido, cpf_ocupante)" +
-                    " VALUES (?, ?, ?, ?)";
+            String SQL = "INSERT INTO bibliotecapublica.emprestimo(data_inicial, vencimento, devolvido, cpf_ocupante, id)" +
+                    " VALUES (?, ?, ?, ?, ?)";
             PreparedStatement comando = conexao.prepareStatement(SQL);
-            comando.setDate(1, Date.valueOf(emprestimo.getDataInicial());
+            comando.setDate(1, Date.valueOf(emprestimo.getDataInicial()));
+            comando.setDate(2, Date.valueOf(emprestimo.getVencimento()));
+            comando.setBoolean(3, emprestimo.getDevolvido());
+            comando.setString(4, emprestimo.getCpfOcupante());
+            comando.setInt(5, emprestimo.getId());
+            retorno = comando.executeUpdate();
         }catch (SQLException e){
             System.out.println("Erro ao emprestar livro");
             e.printStackTrace();
         }
+        return (retorno > 0);
+    }
+
+    public boolean deletaEmprestimo(Emprestimo emprestimo){
+        int retorno = 0;
+        try(Connection conexao = Conexao.getConexao()){
+            String SQL = "DELETE FROM bibliotecapublica.emprestimo WHERE id=?";
+            PreparedStatement comando = conexao.prepareStatement(SQL);
+            comando.setInt(1, emprestimo.getId());
+            retorno = comando.executeUpdate();
+        }catch(SQLException e){
+            System.out.println("Erro ao deletar emprestimo");
+            e.printStackTrace();
+        }
+        return (retorno > 0);
     }
 }
--- src/main/java/Modelo/Emprestimo.java
+++ src/main/java/Modelo/Emprestimo.java
@@ -8,14 +8,17 @@ public class Emprestimo{
     private LocalDate vencimento;
     private String cpfOcupante;
     private boolean devolvido;
+    private int id;
 
     public String getCpfOcupante() { return this.cpfOcupante; }
     public LocalDate getVencimento() { return this.vencimento; }
     public LocalDate getDataInicial() { return this.dataInicial; }
     public boolean getDevolvido(){ return this.devolvido; }
+    public int getId(){ return this.id; }
 
     public void setCpfOcupante(String cpfOcupante){this.cpfOcupante = cpfOcupante; }
     public void setVencimento(LocalDate vencimento){this.vencimento = vencimento; }
     public void setDataInicial(LocalDate dataInicial){this.dataInicial = dataInicial; }
     public void setDevolvido(Boolean devolvido){this.devolvido = devolvido; }
+    public void setId(int id){ this.id = id; }
 }
